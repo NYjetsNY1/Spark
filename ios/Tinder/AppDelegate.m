dd5365cbed3b790768c712045beb1b041adea310
nyjetsny1 <nyjetsny1@gmail.com>
2018-01-31 20:58:36 -0600

    got more facebook integration stuff and build succeeding, don't want to lose it now. Next step is to add the button

--- ios/Tinder/AppDelegate.m
+++ ios/Tinder/AppDelegate.m
@@ -8,6 +8,8 @@
  */
 
 #import "AppDelegate.h"
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
 
 #import "RCTBundleURLProvider.h"
 #import "RCTRootView.h"
@@ -32,6 +34,16 @@
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   return YES;
+}
+
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation {
+  return [[FBSDKApplicationDelegate sharedInstance] application:application
+                                                        openURL:url
+                                              sourceApplication:sourceApplication
+                                                     annotation:annotation];
 }
 
 @end
