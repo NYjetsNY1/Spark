27017985c351ca3b3161e94db4a967e92916366c
nyjetsny1 <nyjetsny1@gmail.com>
2018-02-07 19:33:12 -0600

    changes to shell script

--- myscript.sh
+++ myscript.sh
@@ -7,8 +7,6 @@ npm install react-native-cli
 npm install watchman
 echo Replacing SwipeCards.js
 cp -f $(pwd)/Fixes/SwipeCards.js $(pwd)/node_modules/react-native-swipe-cards/
-echo Replacing UIManager.js
-cp -f $(pwd)/Fixes/UIManager.js $(pwd)/node_modules/react-native/Libraries/Utilities/
 echo Replacing InitializeJavaScriptAppEngine.js
 cp -f $(pwd)/Fixes/InitializeJavaScriptAppEngine.js $(pwd)/node_modules/react-native/Libraries/JavaScriptAppEngine/Initialization/
 echo Deleting Fixes folder initial_setup.txt file
