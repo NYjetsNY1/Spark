9d702ff2921ed257050422bc68188a5361890852
Mimi <Mimi@MacBook-Pro-2.local>
2018-03-21 13:57:31 -0500

    pulling home page cards from db

--- App/components/home.js
+++ App/components/home.js
@@ -17,6 +17,7 @@ import Nav from './global-widgets/nav'
 import SwipeCards from 'react-native-swipe-cards';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Iconz from 'react-native-vector-icons/Ionicons';
+import firebase from '../config/firebase';
 
 
 var image1 = require('../images/image1.jpeg')
@@ -68,16 +69,50 @@ const Cards = [{
     "friends": 12,
     "interests": 44,
     "image": image6
-}]
+}];
 
 export default class Home extends Component {
     constructor(props){
-        super(props)
+        super(props);
         this.state = {
-            cards: Cards
-        }
+            userId: this.props.userData.userId,
+            cards: []
+        };
+        this.componentWillMount = this.componentWillMount.bind(this);
     }
-    Card(x){
+
+    componentWillMount(){
+        let userCards = [];
+        firebase.database().ref().child('users').on('value', users => {
+            let dbUserInfo = users.val();
+            for (var userId in dbUserInfo){
+                console.log(userId);
+                let userObj = dbUserInfo[userId];
+                let userCard = {
+                    "userId": userId,
+                    "name": '',
+                    "age": 0,
+                    "profilePicUrl": '',
+                    "bio": '',
+                    "location": ''
+                };
+                for (var prop in userObj){
+                    if(prop == 'name') userCard.name = userObj[prop];
+                    if(prop == 'age') userCard.age = userObj[prop];
+                    if(prop == 'profilePicUrl') userCard.profilePicUrl = userObj[prop];
+                    if(prop == 'bio') userCard.bio = userObj[prop];
+                    if(prop == 'location') userCard.location = userObj[prop];
+                }
+                userCards.push(userCard);
+                //console.log(userCard);
+            }
+            this.state.cards = userCards;
+            this.setState(this.state);
+            console.log(userCards);
+        });
+    }
+
+    CardBackup(x){
         return (
             <View style={styles.card}>
                 <Image source ={x.image} resizeMode="contain" style ={{width:350, height:350}} />
@@ -89,6 +124,25 @@ export default class Home extends Component {
                     <View style={{flexDirection:'row'}}>
                         <View style={{padding:13,  borderLeftWidth:1,borderColor:'#e3e3e3', alignItems:'center', justifyContent:'space-between'}}><Icon name='people-outline' size={20} color="#777" style={{}} /><Text style={{fontSize:16, fontWeight:'200', color:'#555'}}>{x.friends}</Text></View>
                         <View style={{padding:13, borderLeftWidth:1,borderColor:'#e3e3e3', alignItems:'center', justifyContent:'space-between'}}><Icon name='import-contacts' size={20} color="#777" /><Text style={{fontSize:16, fontWeight:'200', color:'#555'}}>{x.interests}</Text></View>
+                    </View>
+                </View>
+            </View>
+        )
+    }
+
+    Card(x){
+        return (
+            <View style={styles.card}>
+                <Image source ={{uri: x.profilePicUrl}} resizeMode="contain" style ={{width:350, height:350}} />
+                <View style={{width:350, height:70, flexDirection:'row', alignItems:'center', justifyContent:'space-between'}}>
+                    <View style={{flexDirection:'row', margin:15, marginTop:25,}} >
+                        <Text style={{fontSize:20, fontWeight:'300', color:'#444'}}>{x.name}, </Text>
+                        <Text style={{fontSize:21, fontWeight:'200', color:'#444'}}>{x.age}</Text>
+                    </View>
+                </View>
+                <View style={{width:350, height:70, flexDirection:'row', alignItems:'center', justifyContent:'space-between'}}>
+                    <View style={{flexDirection:'row', margin:15, marginTop:25,}} >
+                        <Text style={{fontSize:10, fontWeight:'300', color:'#444'}}>{x.bio}, </Text>
                     </View>
                 </View>
             </View>
@@ -123,6 +177,8 @@ export default class Home extends Component {
                 <Nav chat = {() => this.props.navigator.replace({id: "messages", userData: this.props.userData})}
                      toProfile = {() => this.props.navigator.replace({id:'profile', userData: this.props.userData})} />
                 <SwipeCards
+                    nopeStyle={styles.nopeStyle}
+                    yupStyle={styles.yupStyle}
                     ref = {'swiper'}
                     cards={this.state.cards}
                     containerStyle = {{  backgroundColor: '#f7f7f7', alignItems:'center'}}
@@ -130,7 +186,7 @@ export default class Home extends Component {
                     renderNoMoreCards={() => this.noMore()}
                     handleYup={this.handleYup}
                     handleNope={this.handleNope} />
-                <View style={{flexDirection:'row', alignItems:'center', justifyContent:'center', margin: '-60%'}}>
+                <View style={{flexDirection:'row', alignItems:'center', justifyContent:'center', margin: '-40%'}}>
                     <TouchableOpacity style = {styles.buttons} onPress = {() => this.nope()}>
                         <Iconz name='ios-close' size={45} color="#888" style={{}} />
                     </TouchableOpacity>
@@ -178,7 +234,24 @@ const styles = StyleSheet.create({
         borderWidth:2,
         borderColor:'#e3e3e3',
         width: 350,
-        height: 420,
+        height: 420
+    },
+    nopeStyle: {
+        borderColor: 'red',
+        borderWidth: 2,
+        position: 'absolute',
+        bottom: 200,
+        padding: 20,
+        borderRadius: 5,
+        left: 20,
+    },
+    yupStyle: {
+        borderColor: 'green',
+        borderWidth: 2,
+        position: 'absolute',
+        padding: 20,
+        bottom: 200,
+        borderRadius: 5,
+        right: 20,
     }
-
 });
